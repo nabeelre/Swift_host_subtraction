f36c630351a5ec0f8779572b03654572a98c362e
Nabeel Rehemtulla <nabeelre@gmail.com>
2026-02-04 11:34:30 -0600

    a little more linting

--- SwiftPhotom/commands.py
+++ SwiftPhotom/commands.py
@@ -19,7 +19,9 @@ def uvotimsum(_in, _out, _exclude='none'):
 
 
 def uvotmaghist(_in, _reg, _bgreg, _out, _gif):
-    comm = 'uvotmaghist %s srcreg=%s bkgreg=%s  outfile=%s plotfile=%s coinfile=caldb zerofile=caldb exclude=none chatter=0 clobber=yes logtime=no psffile=caldb apercorr=curveofgrowth' % (_in,_reg,_bgreg,_out,_gif)
+    comm = 'uvotmaghist %s srcreg=%s bkgreg=%s  outfile=%s plotfile=%s ' \
+        'coinfile=caldb zerofile=caldb exclude=none chatter=0 clobber=yes ' \
+        'logtime=no psffile=caldb apercorr=curveofgrowth' % (_in, _reg, _bgreg, _out, _gif)
 
     run(comm)
 
@@ -34,7 +36,7 @@ def fcopy(_in, _out):
     run(comm)
 
 
-#def find_attfile(_infile):
+# def find_attfile(_infile):
 #    obsID=_infile.split('/')[0]
 #    aux=glob.glob(obsID+'/auxil/sw'+obsID+'sat.fits*')
 #    if os.path.isfile(aux[0]):
@@ -42,11 +44,13 @@ def fcopy(_in, _out):
 #    else:
 #        return 0
 #
-#def uvotskycorr(_what,_infile,_corrfile,_attfile,_outfile):
-#    comm = 'uvotskycorr what=%s skyfile=%s corrfile=%s attfile=%s outfile=%s starid="mag.err=5 rot.error=60" catspec=usnob1.spec options=NONE flagfile=NONE' % (_what,_infile,_corrfile,_attfile,_outfile)
+# def uvotskycorr(_what,_infile,_corrfile,_attfile,_outfile):
+#    comm = 'uvotskycorr what=%s skyfile=%s corrfile=%s attfile=%s outfile=%s ' \
+#        'starid="mag.err=5 rot.error=60" catspec=usnob1.spec options=NONE ' \
+#        'flagfile=NONE' % (_what, _infile, _corrfile, _attfile, _outfile)
 #    run(comm)
 #
-#def aspect_correction(_infile):
+# def aspect_correction(_infile):
 #    attfile=find_attfile(_infile)
 #    if not attfile:
 #        print('attfile for '+_infile+'not found. Cannot perform aspect correction.')
